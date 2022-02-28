1eb904b1e351f09458c3c5076f29209e1e2939cf
RafaelBadaro <rafabadaro.15@gmail.com>
2022-02-28 10:50:29 +0100

    Practice 1

---
+++ lab5.1/practice1.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM) # GPIO24
+ 
+def blink(pin): 
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 1)
+    time.sleep(0.5)
+    GPIO.output(pin, 0)
+    time.sleep(0.5)
+
+
+while True:
+    blink(24)
+
+
+GPIO.cleanup()
+print("Program finished")
+
