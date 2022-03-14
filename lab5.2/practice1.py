0186d5e83e65b0db7b00c320c835ccfdfd3b743a
RafaelBadaro <rafabadaro.15@gmail.com>
2022-03-14 11:09:22 +0100

    Ajusting lab5.2 practice 1

--- lab5.2/practice1.py
+++ lab5.2/practice1.py
@@ -7,12 +7,25 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 GPIO.setup(18, GPIO.OUT)
 
+def blink(pin): 
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 1)
+    time.sleep(0.1)
+    GPIO.output(pin, 0)
+
 while True:
     if(GPIO.input(17) == 0):
+        blink(18)
         print("Button release")
-        GPIO.output(18, 0)
         time.sleep(0.3)
     else: 
-        GPIO.output(18, 1)
-        print("Button release")
+        GPIO.output(18, 0)
+        print("Button pressed")
         time.sleep(0.3)
+ 
+
+
+
+GPIO.cleanup()
+print("Program finished")
+
