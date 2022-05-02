70b0fadd164c1d67cf876e474daa66d5875dbeb4
RafaelBadaro <rafabadaro.15@gmail.com>
2022-05-02 14:14:11 +0200

    Adding aditional configurations on the lights and stepper

--- project/script.py
+++ project/script.py
@@ -1,13 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 from multiprocessing import Process
+from datetime import datetime
 GPIO.setmode(GPIO.BCM)
 
-def ultrasound():
-    # Set GPIO's
-    TRIG = 5
-    ECHO = 6
+# Times
+time_begin_night = "20:00:00"
+time_almost_midnight = "23:59:59"
+time_midnight = "00:00:00"
+time_end_night = "06:00:00"
 
+# GPIO's
+
+# ultrasound
+TRIG = 5
+ECHO = 6
+
+# step motor
+IN1 = 23
+IN2 = 22
+IN3 = 27
+IN4 = 17 
+BUTTON_ROTATE_STEPMOTOR = 24
+
+# pump 
+BUTTON_PUMP = 26
+PUMP = 16 # pump
+
+# light 
+BUTTON_LIGHT = 25
+LIGHT = 8 # light
+
+def get_current_time():
+      # TODO - verify time, its getting a strange time  
+      time_now = datetime.now()
+      time_current = time_now.strftime("%H:%M:%S")
+      return time_current
+
+
+def ultrasound():
     print("Distance Measurement In Progress")
 
     GPIO.setup(TRIG,GPIO.OUT)
@@ -52,20 +83,12 @@ def step_motor():
 
     print("Initializing Stepper Feeder")
 
-    in1 = 23
-    in2 = 22
-    in3 = 27
-    in4 = 17 
-    button1 = 24
-
-    # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
     step_sleep = 0.002
     
     step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
     
     direction = False # True for clockwise, False for counter-clockwise
     
-    # defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
     step_sequence = [[1,0,0,1],
                     [1,0,0,0],
                     [1,1,0,0],
@@ -76,27 +99,26 @@ def step_motor():
                     [0,0,0,1]]
     
     # setting up
-    GPIO.setup(in1, GPIO.OUT)
-    GPIO.setup(in2, GPIO.OUT)
-    GPIO.setup(in3, GPIO.OUT)
-    GPIO.setup(in4, GPIO.OUT)
-    GPIO.setup(button1, GPIO.IN)
+    GPIO.setup(IN1, GPIO.OUT)
+    GPIO.setup(IN2, GPIO.OUT)
+    GPIO.setup(IN3, GPIO.OUT)
+    GPIO.setup(IN4, GPIO.OUT)
+    GPIO.setup(BUTTON_ROTATE_STEPMOTOR, GPIO.IN)
     
     # initializing
-    GPIO.output(in1, GPIO.LOW)
-    GPIO.output(in2, GPIO.LOW)
-    GPIO.output(in3, GPIO.LOW)
-    GPIO.output(in4, GPIO.LOW)
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW)
      
-    motor_pins = [in1,in2,in3,in4]
-    motor_step_counter = 0
-    
+    motor_pins = [IN1,IN2,IN3,IN4]
+    motor_step_counter = 0   
     
     def shutdown_leds():
-        GPIO.output(in1, GPIO.LOW)
-        GPIO.output(in2, GPIO.LOW)
-        GPIO.output(in3, GPIO.LOW)
-        GPIO.output(in4, GPIO.LOW)
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.LOW)
 
     def cleanup():
         shutdown_leds()
@@ -104,12 +126,51 @@ def step_motor():
     
     while(True):
 
-      while(GPIO.input(button1) == 1):
-            # Rotate the motor
+      # if (button_change_direction is pressed):
+      #   direction = True
+
+      # Rotate pallet during certain time
+      current_time = get_current_time()
+      if((time_begin_night <= current_time <= time_almost_midnight) or 
+           (time_midnight <= current_time <= time_end_night)):
+            try:
+                i = 0
+                for i in range(1024): # 4096 steps is 360°, so 1024 steps is 90°
+                    for pin in range(0, len(motor_pins)):
+                        GPIO.output(motor_pins[pin], step_sequence[motor_step_counter][pin])
+                    if direction==True:
+                        motor_step_counter = (motor_step_counter - 1) % 8
+                    elif direction==False:
+                        motor_step_counter = (motor_step_counter + 1) % 8
+                    time.sleep(step_sleep)
+            
+            except KeyboardInterrupt:
+                cleanup()
+                exit(1)
+
+      # Rotate forward one pellet
+      if(GPIO.input(BUTTON_ROTATE_STEPMOTOR) == 1):
+            try:
+                i = 0
+                for i in range(1024): # 4096 steps is 360°, so 1024 steps is 90°
+                    for pin in range(0, len(motor_pins)):
+                        GPIO.output(motor_pins[pin], step_sequence[motor_step_counter][pin])
+                    if direction==True:
+                        motor_step_counter = (motor_step_counter - 1) % 8
+                    elif direction==False:
+                        motor_step_counter = (motor_step_counter + 1) % 8
+                    time.sleep(step_sleep)
+            
+            except KeyboardInterrupt:
+                cleanup()
+                exit(1)
+
+      # Rotate while button pressed
+      while(GPIO.input(BUTTON_ROTATE_STEPMOTOR) == 1):
         try:
                 i = 0
                 for i in range(step_count):
-                    if(GPIO.input(button1) == 0): 
+                    if(GPIO.input(BUTTON_ROTATE_STEPMOTOR) == 0): 
                         # If the button is released it stops
                         shutdown_leds()
                         break 
@@ -119,10 +180,6 @@ def step_motor():
                         motor_step_counter = (motor_step_counter - 1) % 8
                     elif direction==False:
                         motor_step_counter = (motor_step_counter + 1) % 8
-                    else: # defensive programming
-                        print( "uh oh... direction should *always* be either True or False" )
-                        cleanup()
-                        exit(1)
                     time.sleep(step_sleep)
             
         except KeyboardInterrupt:
@@ -131,55 +188,70 @@ def step_motor():
         
 
 def relay_pump():
-    button_pump = 26
-    pump = 16 # pump
+
 
     # setting up
-    GPIO.setup(button_pump, GPIO.IN)
-    GPIO.setup(pump, GPIO.OUT)
-    GPIO.output(pump, 1) # Turn off the pump
+    GPIO.setup(BUTTON_PUMP, GPIO.IN)
+    GPIO.setup(PUMP, GPIO.OUT)
+    GPIO.output(PUMP, 1) # Turn off the pump
 
     while True:
-        if(GPIO.input(button_pump) == 0):    
-            GPIO.output(pump, 0) # Turn on 
+        if(GPIO.input(BUTTON_PUMP) == 0):    
+            GPIO.output(PUMP, 0) # Turn on 
             print("Pump status: ON") 
-        elif (GPIO.input(pump) == 0):     
-            GPIO.output(pump, 1) # Turn off 
+        elif (GPIO.input(PUMP) == 0):     
+            GPIO.output(PUMP, 1) # Turn off 
             print("Pump status: OFF")
 
         time.sleep(0.3) 
     
 
-
 def relay_lights():
-    button_light = 25
-    light = 8 # light
 
-     # setting up
-    GPIO.setup(button_light, GPIO.IN)
-    GPIO.setup(light, GPIO.OUT)
-    GPIO.output(light, 1) # Turn off the light
+    isDuringPredefinedTime = False
+
+    # setting up
+    GPIO.setup(BUTTON_LIGHT, GPIO.IN)
+    GPIO.setup(LIGHT, GPIO.OUT)
+    GPIO.output(LIGHT, 1) # Turn off the light
 
     while True:
-        if(GPIO.input(button_light) == 0):            
-              if(GPIO.input(light) == 1):  # If light is off
-                  GPIO.output(light, 0) # Turn on 
+        # Turn lights on based on time
+        current_time = get_current_time()
+        if((time_begin_night <= current_time <= time_almost_midnight) or 
+           (time_midnight <= current_time <= time_end_night)):
+            # If time is between the predefined times, turn on light
+            GPIO.output(LIGHT, 0) 
+            isDuringPredefinedTime = True
+            # TODO - what is the behaviour if the button is pressed during predefined times?
+        elif (isDuringPredefinedTime):
+            # If time is not in between the predefined times 
+            # and the light was turned on during predefined times, turn off light
+            GPIO.output(LIGHT, 1)
+            isDuringPredefinedTime = False
+
+        # Button to turn light on and off
+        if(GPIO.input(BUTTON_LIGHT) == 0):            
+              if(GPIO.input(LIGHT) == 1):  # If light is off
+                  GPIO.output(LIGHT, 0) # Turn on 
                   print("Light status: ON") 
-              elif(GPIO.input(light) == 0): # If light is on
-                  GPIO.output(light, 1) # Turn off 
+              elif(GPIO.input(LIGHT) == 0): # If light is on
+                  GPIO.output(LIGHT, 1) # Turn off 
                   print("Light status: OFF")
               time.sleep(0.3) 
 
 
+
+
 # Execute the methods
 p1 = Process(target=ultrasound)
-p1.start()
+#p1.start()
 p2 = Process(target=step_motor)
 p2.start()
 p3 = Process(target=relay_pump)
-p3.start()
+#p3.start()
 p4 = Process(target=relay_lights)
-p4.start()
+#p4.start()
 p1.join()
 p2.join()
 p3.join()
